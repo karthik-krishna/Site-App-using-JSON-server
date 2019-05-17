bb192eb163f3519975d44197ea267c93a32ecaf0
Karthik Bhilwadkar <karthik.bhilwadkar@askida.lan>
2019-05-17 09:30:59 -0400

    added travis yml

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -4,8 +4,4 @@ export class AppPage {
   navigateTo() {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
-
-  getTitleText() {
-    return element(by.css('app-root span')).getText() as Promise<string>;
-  }
 }
